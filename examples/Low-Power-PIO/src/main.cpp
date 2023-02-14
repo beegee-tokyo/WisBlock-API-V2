d55aa198783be5a4b0204b74a1e767069969077b
beegee-tokyo <bernd@giesecke.tk>
2023-02-14 20:27:43 +0800

    Add Low-Power example for RAK4631

---
+++ examples/Low-Power-PIO/src/main.cpp
@@ -0,0 +1,232 @@
+/**
+ * @file main.cpp
+ * @author Bernd Giesecke (bernd@giesecke.tk)
+ * @brief Low power test
+ * @version 0.1
+ * @date 2023-02-14
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include <Arduino.h>
+#include <WisBlock-API.h>
+
+// Debug output set to 0 to disable app debug output
+#ifndef MY_DEBUG
+#define MY_DEBUG 1
+#endif
+
+#if MY_DEBUG > 0
+#define MYLOG(tag, ...)                     \
+	do                                      \
+	{                                       \
+		if (tag)                            \
+			PRINTF("[%s] ", tag);           \
+		PRINTF(__VA_ARGS__);                \
+		PRINTF("\n");                       \
+		if (g_ble_uart_is_connected)        \
+		{                                   \
+			g_ble_uart.printf(__VA_ARGS__); \
+			g_ble_uart.printf("\n");        \
+		}                                   \
+	} while (0)
+#else
+#define MYLOG(...)
+#endif
+
+/** Define the version of your SW */
+#define SW_VERSION_1 1 // major version increase on API change / not backwards compatible
+#define SW_VERSION_2 0 // minor version increase on API change / backward compatible
+#define SW_VERSION_3 0 // patch version increase on bugfix, no affect on API
+
+/** Application function definitions */
+void setup_app(void);
+bool init_app(void);
+void app_event_handler(void);
+void ble_data_handler(void) __attribute__((weak));
+void lora_data_handler(void);
+
+/** Send Fail counter **/
+uint8_t send_fail = 0;
+
+/** Set the device name, max length is 10 characters */
+char g_ble_dev_name[10] = "RAK-LP";
+
+/**
+ * @brief Initial setup of the application (before LoRaWAN and BLE setup)
+ *
+ */
+void setup_app(void)
+{
+	Serial.begin(115200);
+	time_t serial_timeout = millis();
+	// On nRF52840 the USB serial is not available immediately
+	while (!Serial)
+	{
+		if ((millis() - serial_timeout) < 5000)
+		{
+			delay(100);
+			digitalWrite(LED_GREEN, !digitalRead(LED_GREEN));
+		}
+		else
+		{
+			break;
+		}
+	}
+	digitalWrite(LED_GREEN, LOW);
+
+	// Set firmware version
+	api_set_version(SW_VERSION_1, SW_VERSION_2, SW_VERSION_3);
+}
+
+/**
+ * @brief Final setup of application  (after LoRaWAN and BLE setup)
+ *
+ * @return true
+ * @return false
+ */
+bool init_app(void)
+{
+	MYLOG("APP", "init_app");
+	pinMode(WB_IO2, OUTPUT);
+	digitalWrite(WB_IO2, LOW);
+	return true;
+}
+
+/**
+ * @brief Handle events
+ * 		Events can be
+ * 		- timer (setup with AT+SENDINT=xxx)
+ * 		- interrupt events
+ * 		- wake-up signals from other tasks
+ */
+void app_event_handler(void)
+{
+	// Timer triggered event
+	if ((g_task_event_type & STATUS) == STATUS)
+	{
+		g_task_event_type &= N_STATUS;
+		MYLOG("APP", "Timer wakeup");
+		if (g_lpwan_has_joined)
+		{
+			// Dummy packet
+			uint8_t dummy_packet[] = {0x10, 0x00, 0x00, 0x55};
+
+			lmh_error_status result = send_lora_packet(dummy_packet, 4, 2);
+			switch (result)
+			{
+			case LMH_SUCCESS:
+				MYLOG("APP", "Packet enqueued");
+				break;
+			case LMH_BUSY:
+				MYLOG("APP", "LoRa transceiver is busy");
+				break;
+			case LMH_ERROR:
+				MYLOG("APP", "Packet error, too big to send with current DR");
+				break;
+			}
+		}
+		else
+		{
+			MYLOG("APP", "Network not joined, skip sending");
+		}
+	}
+}
+
+/**
+ * @brief Handle BLE events
+ *
+ */
+void ble_data_handler(void)
+{
+	if (g_enable_ble)
+	{
+		/**************************************************************/
+		/**************************************************************/
+		/// \todo BLE UART data arrived
+		/// \todo or forward them to the AT command interpreter
+		/// \todo parse them here
+		/**************************************************************/
+		/**************************************************************/
+		if ((g_task_event_type & BLE_DATA) == BLE_DATA)
+		{
+			MYLOG("AT", "RECEIVED BLE");
+			// BLE UART data arrived
+			// in this example we forward it to the AT command interpreter
+			g_task_event_type &= N_BLE_DATA;
+
+			while (g_ble_uart.available() > 0)
+			{
+				at_serial_input(uint8_t(g_ble_uart.read()));
+				delay(5);
+			}
+			at_serial_input(uint8_t('\n'));
+		}
+	}
+}
+
+/**
+ * @brief Handle LoRa events
+ *
+ */
+void lora_data_handler(void)
+{
+	// LoRa Join finished handling
+	if ((g_task_event_type & LORA_JOIN_FIN) == LORA_JOIN_FIN)
+	{
+		g_task_event_type &= N_LORA_JOIN_FIN;
+		if (g_join_result)
+		{
+			MYLOG("APP", "Successfully joined network");
+		}
+		else
+		{
+			MYLOG("APP", "Join network failed");
+			/// \todo here join could be restarted.
+			// lmh_join();
+		}
+	}
+
+	// LoRa data handling
+	if ((g_task_event_type & LORA_DATA) == LORA_DATA)
+	{
+		/**************************************************************/
+		/**************************************************************/
+		/// \todo LoRa data arrived
+		/// \todo parse them here
+		/**************************************************************/
+		/**************************************************************/
+		g_task_event_type &= N_LORA_DATA;
+		MYLOG("APP", "Received package over LoRa");
+		char log_buff[g_rx_data_len * 3] = {0};
+		uint8_t log_idx = 0;
+		for (int idx = 0; idx < g_rx_data_len; idx++)
+		{
+			sprintf(&log_buff[log_idx], "%02X ", g_rx_lora_data[idx]);
+			log_idx += 3;
+		}
+		MYLOG("APP", "%s", log_buff);
+	}
+
+	// LoRa TX finished handling
+	if ((g_task_event_type & LORA_TX_FIN) == LORA_TX_FIN)
+	{
+		g_task_event_type &= N_LORA_TX_FIN;
+
+		MYLOG("APP", "LPWAN TX cycle %s", g_rx_fin_result ? "finished ACK" : "failed NAK");
+
+		if (!g_rx_fin_result)
+		{
+			// Increase fail send counter
+			send_fail++;
+
+			if (send_fail == 10)
+			{
+				// Too many failed sendings, reset node and try to rejoin
+				delay(100);
+				api_reset();
+			}
+		}
+	}
+}
