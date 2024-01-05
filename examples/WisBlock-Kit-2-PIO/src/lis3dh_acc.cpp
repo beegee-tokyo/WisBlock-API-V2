d3a0a3c94baf85cebcf1980a9782fe2b14170323
beegee-tokyo <bernd@giesecke.tk>
2024-01-05 14:48:45 +0800

    Add WisBlock Kit 2 as PIO project

---
+++ examples/WisBlock-Kit-2-PIO/src/lis3dh_acc.cpp
@@ -0,0 +1,137 @@
+/**
+ * @file lis3dh_acc.ino
+ * @author Bernd Giesecke (bernd.giesecke@rakwireless.com)
+ * @brief 3-axis accelerometer functions
+ * @version 0.1
+ * @date 2020-07-24
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+// #include "main.h"
+#include <Arduino.h>
+#include <Wire.h>
+#include <SparkFunLIS3DH.h> //http://librarymanager/All#SparkFun-LIS3DH
+
+#define INT1_PIN WB_IO3
+
+void acc_int_callback(void);
+uint16_t *read_acc(void);
+
+/** The LIS3DH sensor */
+LIS3DH acc_sensor(I2C_MODE, 0x18);
+
+/**
+ * @brief Initialize LIS3DH 3-axis 
+ * acceleration sensor
+ * 
+ * @return true If sensor was found and is initialized
+ * @return false If sensor initialization failed
+ */
+bool init_acc(void)
+{
+	// Setup interrupt pin
+	pinMode(INT1_PIN, INPUT);
+
+	Wire.begin();
+
+	acc_sensor.settings.accelSampleRate = 10; //Hz.  Can be: 0,1,10,25,50,100,200,400,1600,5000 Hz
+	acc_sensor.settings.accelRange = 2;		  //Max G force readable.  Can be: 2, 4, 8, 16
+
+	acc_sensor.settings.adcEnabled = 0;
+	acc_sensor.settings.tempEnabled = 0;
+	acc_sensor.settings.xAccelEnabled = 1;
+	acc_sensor.settings.yAccelEnabled = 1;
+	acc_sensor.settings.zAccelEnabled = 1;
+
+	if (acc_sensor.begin() != 0)
+	{
+		return false;
+	}
+
+	uint8_t data_to_write = 0;
+	//************************************************************************/
+	// Below functions can be used to enable the ACC's IRQ and trigger
+	// Sending a packet on movement.
+	//************************************************************************/
+	// // Enable interrupts
+	// data_to_write |= 0x20;                   //Z high
+	// data_to_write |= 0x08;                   //Y high
+	// data_to_write |= 0x02;                   //X high
+	// acc_sensor.writeRegister(LIS3DH_INT1_CFG, data_to_write); // Enable interrupts on high tresholds for x, y and z
+
+	// // Set interrupt trigger range
+	// data_to_write = 0;
+	// // data_to_write |= 0x10;                    // 1/8 range
+	// // acc_sensor.writeRegister(LIS3DH_INT1_THS, data_to_write); // 1/8th range
+	// data_to_write |= 0x08;                   // 1/16 range
+	// acc_sensor.writeRegister(LIS3DH_INT1_THS, data_to_write); // 1/16th range
+
+	// // Set interrupt signal length
+	// data_to_write = 0;
+	// data_to_write |= 0x01; // 1 * 1/50 s = 20ms
+	// acc_sensor.writeRegister(LIS3DH_INT1_DURATION, data_to_write);
+
+	// acc_sensor.readRegister(&data_to_write, LIS3DH_CTRL_REG5);
+	// data_to_write &= 0xF3;                    //Clear bits of interest
+	// data_to_write |= 0x08;                    //Latch interrupt (Cleared by reading int1_src)
+	// acc_sensor.writeRegister(LIS3DH_CTRL_REG5, data_to_write); // Set interrupt to latching
+
+	// // Select interrupt pin 1
+	// data_to_write = 0;
+	// data_to_write |= 0x40; //AOI1 event (Generator 1 interrupt on pin 1)
+	// data_to_write |= 0x20; //AOI2 event ()
+	// acc_sensor.writeRegister(LIS3DH_CTRL_REG3, data_to_write);
+
+	// // No interrupt on pin 2
+	// acc_sensor.writeRegister(LIS3DH_CTRL_REG6, 0x00);
+
+	// Enable high pass filter
+	acc_sensor.writeRegister(LIS3DH_CTRL_REG2, 0x01);
+
+	// Set low power mode
+	data_to_write = 0;
+	acc_sensor.readRegister(&data_to_write, LIS3DH_CTRL_REG1);
+	data_to_write |= 0x08;
+	acc_sensor.writeRegister(LIS3DH_CTRL_REG1, data_to_write);
+	delay(100);
+
+	data_to_write = 0;
+	acc_sensor.readRegister(&data_to_write, 0x1E);
+	data_to_write |= 0x90;
+	acc_sensor.writeRegister(0x1E, data_to_write);
+	delay(100);
+
+	//************************************************************************/
+	// Below functions can be used to enable the ACC's IRQ and trigger
+	// Sending a packet on movement.
+	//************************************************************************/
+	// clear_acc_int();
+
+	// // Set the interrupt callback function
+	// attachInterrupt(INT1_PIN, acc_int_callback, RISING);
+
+	read_acc();
+
+	return true;
+}
+
+uint16_t acc[3] = {0};
+
+uint16_t *read_acc(void)
+{
+	acc[0] = (int16_t)(acc_sensor.readFloatAccelX() * 1000.0);
+	acc[1] = (int16_t)(acc_sensor.readFloatAccelY() * 1000.0);
+	acc[2] = (int16_t)(acc_sensor.readFloatAccelZ() * 1000.0);
+	return acc;
+}
+
+/**
+ * @brief Clear ACC interrupt register to enable next wakeup
+ * 
+ */
+void clear_acc_int(void)
+{
+	uint8_t data_read;
+	acc_sensor.readRegister(&data_read, LIS3DH_INT1_SRC);
+}
---
+++ examples/WisBlock-Kit-2-PIO/src/main.h
@@ -0,0 +1,168 @@
+/**
+ * @file main.h
+ * @author Bernd Giesecke (bernd.giesecke@rakwireless.com)
+ * @brief For application specific includes and definitions
+ *        Will be included from main.h
+ * @version 0.1
+ * @date 2021-04-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef APP_H
+#define APP_H
+
+#include <Arduino.h>
+/** Add you required includes after Arduino.h */
+#include <Wire.h>
+#include "TinyGPS++.h" //http://librarymanager/All#TinyGPSPlus
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME680.h> // Click to install library: http://librarymanager/All#Adafruit_BME680
+
+// Debug output set to 0 to disable app debug output
+#ifndef MY_DEBUG
+#define MY_DEBUG 1
+#endif
+
+/** Examples for application events */
+#define ACC_TRIGGER 0b1000000000000000
+#define N_ACC_TRIGGER 0b0111111111111111
+
+#ifdef NRF52_SERIES
+#if MY_DEBUG > 0
+#define MYLOG(tag, ...)                     \
+	do                                      \
+	{                                       \
+		if (tag)                            \
+			PRINTF("[%s] ", tag);           \
+		PRINTF(__VA_ARGS__);                \
+		PRINTF("\n");                       \
+		if (g_ble_uart_is_connected)        \
+		{                                   \
+			g_ble_uart.printf(__VA_ARGS__); \
+			g_ble_uart.printf("\n");        \
+		}                                   \
+	} while (0)
+#else
+#define MYLOG(...)
+#endif
+#endif
+#ifdef ARDUINO_ARCH_RP2040
+#if MY_DEBUG > 0
+#define MYLOG(tag, ...)                  \
+	do                                   \
+	{                                    \
+		if (tag)                         \
+			Serial.printf("[%s] ", tag); \
+		Serial.printf(__VA_ARGS__);      \
+		Serial.printf("\n");             \
+	} while (0)
+#else
+#define MYLOG(...)
+#endif
+#endif
+#if defined ESP32
+#if MY_DEBUG > 0
+#define MYLOG(tag, ...)                                                 \
+	if (tag)                                                            \
+		Serial.printf("[%s] ", tag);                                    \
+	Serial.printf(__VA_ARGS__);                                         \
+	Serial.printf("\n");                                                \
+	if (g_ble_uart_is_connected)                                        \
+	{                                                                   \
+		char buff[255];                                                 \
+		int len = sprintf(buff, __VA_ARGS__);                           \
+		uart_tx_characteristic->setValue((uint8_t *)buff, (size_t)len); \
+		uart_tx_characteristic->notify(true);                           \
+		delay(50);                                                      \
+	}
+#else
+#define MYLOG(...)
+#endif
+#endif
+
+/** Application function definitions */
+void setup_app(void);
+bool init_app(void);
+void app_event_handler(void);
+void ble_data_handler(void) __attribute__((weak));
+void lora_data_handler(void);
+
+/** Temperature + Humidity stuff */
+bool init_bme(void);
+bool read_bme(void);
+void start_bme(void);
+
+/** Accelerometer stuff */
+bool init_acc(void);
+void clear_acc_int(void);
+uint16_t *read_acc(void);
+
+// GNSS functions
+bool init_gnss(void);
+bool poll_gnss(void);
+
+// LoRaWan functions
+/** Include the WisBlock-API-V2 */
+#include <WisBlock-API-V2.h> // Click to install library: http://librarymanager/All#WisBlock-API-V2
+
+struct tracker_data_s
+{
+	uint8_t data_flag1 = 0x01;	// 1
+	uint8_t data_flag2 = 0x88;	// 2
+	uint8_t lat_1 = 0;			// 3
+	uint8_t lat_2 = 0;			// 4
+	uint8_t lat_3 = 0;			// 5
+	uint8_t long_1 = 0;			// 6
+	uint8_t long_2 = 0;			// 7
+	uint8_t long_3 = 0;			// 8
+	uint8_t alt_1 = 0;			// 9
+	uint8_t alt_2 = 0;			// 10
+	uint8_t alt_3 = 0;			// 11
+	uint8_t data_flag3 = 0x08;	// 12
+	uint8_t data_flag4 = 0x02;	// 13
+	uint8_t batt_1 = 0;			// 14
+	uint8_t batt_2 = 0;			// 15
+	uint8_t data_flag5 = 0x03;	// 16
+	uint8_t data_flag6 = 0x71;	// 17
+	int8_t acc_x_1 = 0;			// 18
+	int8_t acc_x_2 = 0;			// 19
+	int8_t acc_y_1 = 0;			// 20
+	int8_t acc_y_2 = 0;			// 21
+	int8_t acc_z_1 = 0;			// 22
+	int8_t acc_z_2 = 0;			// 23
+	uint8_t data_flag7 = 0x07;	// 24
+	uint8_t data_flag8 = 0x68;	// 25
+	uint8_t humid_1 = 0;		// 26
+	uint8_t data_flag9 = 0x02;	// 27
+	uint8_t data_flag10 = 0x67; // 28
+	uint8_t temp_1 = 0;			// 29
+	uint8_t temp_2 = 0;			// 30
+	uint8_t data_flag11 = 0x06; // 31
+	uint8_t data_flag12 = 0x73; // 32
+	uint8_t press_1 = 0;		// 33
+	uint8_t press_2 = 0;		// 34
+	uint8_t data_flag13 = 0x04; // 35
+	uint8_t data_flag14 = 0x02; // 36
+	uint8_t gas_1 = 0;			// 37
+	uint8_t gas_2 = 0;			// 38
+};
+extern tracker_data_s g_tracker_data;
+#define TRACKER_DATA_LEN 38		  // sizeof(g_tracker_data) with BME680
+#define TRACKER_DATA_SHORT_LEN 23 // sizeof(g_tracker_data) without BME680
+
+/** Battery level uinion */
+union batt_s
+{
+	uint16_t batt16 = 0;
+	uint8_t batt8[2];
+};
+/** Latitude/Longitude value union */
+union latLong_s
+{
+	uint32_t val32;
+	uint8_t val8[4];
+};
+
+#endif
